161532cc6e1787938bf8b1b18c564fd3815a7f7d
ABISHEK_KU <abishekku.95@gmail.com>
2025-10-01 09:09:47 +0530

    fixes the error while building application
    - fix the error the profile page while building the application.

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,32 +1,46 @@
-import { getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing } from "@/actions/profile.action";
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile.action";
 import { notFound } from "next/navigation";
 import ProfilePageClient from "./ProfilePageClient";
 
 interface UserName {
-  params: { username: string };
+  params: Promise<{ username: string }>;
 }
 
 export async function generateMetadata({ params }: UserName) {
-  const user = await getProfileByUsername(params.username);
+  const { username } = await params;
+  const user = await getProfileByUsername(username);
   if (!user) {
     return;
   }
 
   return {
-    title: `Profile |${user.name ?? params.username}`,
-    description: user.bio || `Check out ${params.username}'s profile.`,
+    title: `Profile |${user.name ?? username}`,
+    description: user.bio || `Check out ${username}'s profile.`,
   };
 }
 export default async function ProfilePageServer({ params }: UserName) {
- const user = await getProfileByUsername(params.username);
- if (!user) {
-  notFound();
- }
- const [posts,likedPosts,isCurrentUserFollowing]=await Promise.all([
-  getUserPosts(user.id),
-  getUserLikedPosts(user.id),
-  isFollowing(user.id)
- ])
+  const { username } = await params;
+  const user = await getProfileByUsername(username);
+  if (!user) {
+    notFound();
+  }
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  return <ProfilePageClient user={user} posts={posts} likedPosts={likedPosts} isFollowing={isCurrentUserFollowing}/>;
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
 }
