bd3894267dbf280dde95b28075aa7097552ff744
ABISHEK_KU <abishekku.95@gmail.com>
2025-10-01 13:00:51 +0530

    fix the user sync error
    - fix the user sync error while signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,32 @@
 import { getPosts } from "@/actions/post.action";
-import { getDbUserId } from "@/actions/user.action";
+import { getDbUserId, syncUser } from "@/actions/user.action";
 import CreatePost from "@/components/CreatePost";
 import PostCard from "@/components/PostCard";
 import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
-  const user=await currentUser();
-  const posts=await getPosts();
-  const dbUserId=await getDbUserId();
+  const user = await currentUser();
+  const posts = await getPosts();
+
+  if (user) {
+    await syncUser(); // Ensure user is synced in DB before fetching dbUserId
+  }
+
+  const dbUserId = await getDbUserId();
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
       <div className="lg:col-span-6">
-        {user?<CreatePost />:null}
-        <div className="space-y-6">{
-          posts?.map((post)=>(
-            <PostCard key={post.id} post={post} dbUserId={dbUserId??""}/>
-          ))}</div>
+        {user ? <CreatePost /> : null}
+        <div className="space-y-6">
+          {posts?.map((post) => (
+            <PostCard key={post.id} post={post} dbUserId={dbUserId ?? ""} />
+          ))}
+        </div>
       </div>
       <div className="hidden lg:block lg:col-span-4 top-20">
-        <WhoToFollow/>
+        <WhoToFollow />
       </div>
     </div>
   );
